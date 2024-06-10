12bd5a76db521cd92ffc1814ca012d0fb52e868c
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2024-06-10 15:54:41 +0530

    cimmit

---
+++ lib/screens/leads/getNewLeads.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetNewLeads extends StatelessWidget {
+  const GetNewLeads({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
