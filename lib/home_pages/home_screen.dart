99331b8a73708da9be55d2c61d4ebd2d3d3e2798
shiva <mohitmeshram04101999@gmail.com>
2024-06-11 08:56:54 +0530

    changes in widget

--- lib/home_pages/home_screen.dart
+++ lib/home_pages/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:leadkart/component/HelpButtonWhite.dart';
+import 'package:leadkart/component/addRequirmentTile.dart';
+import 'package:leadkart/my%20custom%20assets%20dart%20file/actionButton.dart';
 import 'package:leadkart/helper/dimention.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -88,175 +91,151 @@ class _HomeScreenState extends State<HomeScreen> {
 
           SizedBox(width: SC.from_height(60)),
 
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.call_outlined,size:  SC.from_height(17),),
-                SizedBox(width: SC.from_height(4),),
-                Center(child: Text('Help ?',style: TextStyle(fontSize: SC.from_height(16),color: Colors.white),),),
-              ],
-            ),
-            width: SC.from_height(85),
-            height: SC.from_height(30),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(SC.from_height(15)),border: Border.all(
-                color: Colors.white
-            )),
-          ),
+          HelpButton(),
+
+          // Container(
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       Icon(Icons.call_outlined,size:  SC.from_height(17),),
+          //       SizedBox(width: SC.from_height(4),),
+          //       Center(child: Text('Help ?',style: TextStyle(fontSize: SC.from_height(16),color: Colors.white),),),
+          //     ],
+          //   ),
+          //   width: SC.from_height(85),
+          //   height: SC.from_height(30),
+          //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(SC.from_height(15)),border: Border.all(
+          //       color: Colors.white
+          //   )),
+          // ),
           SizedBox(width: SC.from_height(20)),
         ],
       ),
-      body: Padding(
-        padding:   EdgeInsets.symmetric(horizontal:SC.from_height(18) ),
-        child: ListView(
-          children: [
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal:SC.from_height(18) ),
+        children: [
 
-            // SelectPlanTile(),
+          // SelectPlanTile(),
 
-            SizedBox(height: SC.from_height(15),),
+          SizedBox(height: SC.from_height(15),),
 
-            Text('Introducing AI-powered ads with Leadkart',style: TextStyle(fontSize: SC.from_height(17)),),
+          Text('Introducing AI-powered ads with Leadkart',style: TextStyle(fontSize: SC.from_height(17)),),
 
-            SizedBox(height: SC.from_height(8),),
+          SizedBox(height: SC.from_height(8),),
 
-            GridView.builder(
-              primary: false,
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: SC.from_width(7),
-                crossAxisSpacing: SC.from_width(7),
-                mainAxisExtent: SC.from_width(50),
-              ),
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return  Container(
-                  margin: EdgeInsets.symmetric(horizontal: SC.from_width(5)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 1,
-                        child: Container(
-                          padding: EdgeInsets.all(SC.from_width(8)),
-                          // decoration: BoxDecoration(border: Border.all()),
-                          child: Image.asset(
-                            item['image']!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: SC.from_width(3)), // Add some spacing between image and text
-                      Expanded(
-                        child: Text(
-                          item['text']!,
-                          style: TextStyle(
-                            fontSize: SC.from_width(15),
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1, // Adjust maxLines if you want to limit the number of lines for text
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-
-              },
+          GridView.builder(
+            primary: false,
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: SC.from_width(7),
+              crossAxisSpacing: SC.from_width(7),
+              mainAxisExtent: SC.from_width(50),
             ),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return  Container(
+                margin: EdgeInsets.symmetric(horizontal: SC.from_width(5)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(SC.from_width(8)),
+                        // decoration: BoxDecoration(border: Border.all()),
+                        child: Image.asset(
+                          item['image']!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: SC.from_width(3)), // Add some spacing between image and text
+                    Expanded(
+                      child: Text(
+                        item['text']!,
+                        style: TextStyle(
+                          fontSize: SC.from_width(15),
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1, // Adjust maxLines if you want to limit the number of lines for text
+                      ),
+                    ),
+                  ],
+                ),
+              );
 
-            SizedBox(height: SC.from_height(15),),
+            },
+          ),
 
-            Container(
+          SizedBox(height: SC.from_height(15),),
+
+          Container(
+            clipBehavior: Clip.hardEdge,
+            child:
+            Image.asset('assets/home_images/img_3.png',fit: BoxFit.cover,),
+            width: SC.from_height(250),
+            height: SC.from_height(200),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(SC.from_height(8)),),
+          ),
+
+          SizedBox(height: SC.from_height(15),),
+
+          Image.asset('assets/home_images/4.png',fit: BoxFit.cover,),
+
+          SizedBox(height: SC.from_height(15),),
+
+          Text('Choose your Ad requirement',style: TextStyle(fontSize:  SC.from_height(16)),),
+
+          SizedBox(height: SC.from_height(15),),
+
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context,index){
+              return Container(margin: EdgeInsets.only(top: SC.from_height(8),left: SC.from_height(2),right: SC.from_height(2)),
+                width: double.infinity,
+                height: SC.from_height(85),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  // border: Border.all(),
+                  borderRadius: BorderRadius.circular(SC.from_height(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.10), // Shadow color with opacity
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 3, // Blur radius
+                      offset: Offset(0, 4), // Offset in x and y directions
+                    ),
+                  ],
+                ),
+                child: AddREquirmentTile(icon:Icon(iconList[index]),),
+              );
+            },
+            itemCount: 4,
+          ),
+
+          SizedBox(height: SC.from_height(20),),
+
+          Container(
+              height: SC.from_height(134),
               clipBehavior: Clip.hardEdge,
-              child:
-              Image.asset('assets/home_images/img_3.png',fit: BoxFit.cover,),
-              width: SC.from_height(250),
-              height: SC.from_height(200),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(SC.from_height(8)),),
-            ),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              child: Image.asset('assets/home_images/img_4.png',fit: BoxFit.cover,)),
 
-            SizedBox(height: SC.from_height(15),),
+          SizedBox(height: SC.from_height(20),),
 
-            Image.asset('assets/home_images/4.png',fit: BoxFit.cover,),
 
-            SizedBox(height: SC.from_height(15),),
-
-            Text('Choose your Ad requirement',style: TextStyle(fontSize:  SC.from_height(16)),),
-
-            SizedBox(height: SC.from_height(15),),
-
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context,index){
-                return Container(margin: EdgeInsets.only(top: SC.from_height(8),left: SC.from_height(2),right: SC.from_height(2)),
-                  width: double.infinity,
-                  height: SC.from_height(85),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    // border: Border.all(),
-                    borderRadius: BorderRadius.circular(SC.from_height(8)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.10), // Shadow color with opacity
-                        spreadRadius: 2, // Spread radius
-                        blurRadius: 3, // Blur radius
-                        offset: Offset(0, 4), // Offset in x and y directions
-                      ),
-                    ],
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      child: Center(child: Icon(iconList[index % iconList.length])),
-
-                      backgroundColor: Color.fromRGBO(241, 241, 241, 1),
-                      radius:SC.from_height(25),
-                    ),
-                    title: Text('Get Leads from Social Platforms',maxLines: 1,overflow: TextOverflow.ellipsis,),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('Acquire customers from insta, fb'),
-                        SizedBox(height: SC.from_height(5),),
-                        Row(
-                          children: [
-                            Container(
-                              width: SC.from_height(16),
-                              height: SC.from_height(16),
-                              child: Image.asset('assets/facebook.png'),
-                            ),
-                            SizedBox(width: SC.from_height(8),),
-                            Container(
-                              width: SC.from_height(20),
-                              height: SC.from_height(20),
-                              child: Image.asset('assets/google.png'),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    trailing: Icon(Icons.arrow_forward_ios_rounded),
-                  ),
-                );
+          MyactionButton(
+              onActionComplit: (v){},
+              action: ()async{
+                await Future.delayed(Duration(seconds: 3));
               },
-              itemCount: 4,
-            ),
+              duretion: Duration(milliseconds: 300))
 
-            SizedBox(height: SC.from_height(20),),
-
-            Container(
-                height: SC.from_height(134),
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                child: Image.asset('assets/home_images/img_4.png',fit: BoxFit.cover,)),
-
-            SizedBox(height: SC.from_height(20),),
-
-          ],
-        ),
+        ],
       ),
     );
   }
